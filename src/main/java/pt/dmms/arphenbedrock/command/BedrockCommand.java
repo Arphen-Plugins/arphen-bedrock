8f3636c154c2e0a629a6c7f495c90de42fc6bc0c
sousa <diogommsousa2002@gmail.com>
2023-08-18 19:28:02 +0100

    add command for reloading configuration files

--- src/main/java/pt/dmms/arphenbedrock/command/BedrockCommand.java
+++ src/main/java/pt/dmms/arphenbedrock/command/BedrockCommand.java
@@ -1,8 +1,14 @@
 package pt.dmms.arphenbedrock.command;
 
 import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Default;
+import org.bukkit.command.CommandSender;
 import pt.dmms.arphenbedrock.ArphenBedrock;
 
+@CommandAlias("arphenbedrock")
+@CommandPermission("arphenbedrock.admin")
 public class BedrockCommand extends BaseCommand {
 
     private final ArphenBedrock plugin;
@@ -10,4 +16,10 @@ public class BedrockCommand extends BaseCommand {
     public BedrockCommand(ArphenBedrock plugin) {
         this.plugin = plugin;
     }
+
+    @Default
+    public void onCommand(CommandSender sender) {
+        plugin.reloadConfig(sender);
+    }
+
 }
